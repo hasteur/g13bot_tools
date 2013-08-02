08c8843bf8c6741381503291ae4c4233cc879eac
Stephen Lecheler <stephen.lecheler@gmail.com>
2013-08-02 11:20:08 +0000

    Add the periodic g13_db_maintenance script that cleans up the DB Removes:   Deleted Submissions   Redirected Submissions   Submissions that don't have my CSD tag on it any more

--- g13_db_maintenance.py
+++ g13_db_maintenance.py
@@ -21,7 +21,7 @@ __version__ = '$Id$'
 # Distributed under the terms of the MIT license.
 #
 
-import os, re, pickle, bz2, time, datetime, sqlite3
+import os, re, pickle, bz2, time, datetime, sqlite3, logging
 import wikipedia as pywikibot
 from pywikibot import i18n
 
@@ -84,7 +84,9 @@ def g13_db_maintenance():
     Go through the local DB and clean up any records that are no longer 
     appropriate
   """
+  global logger
   conn = sqlite3.connect('g13.db')
+  logger.debug('Opened DB conn')
   cur = conn.cursor()
   cur.execute( \
     "SELECT article, editor" + \
@@ -102,31 +104,64 @@ def g13_db_maintenance():
     )
     if False == article.exists():
         #Submission doesn't exisist any more, Remove it from the DB
+        logger.info('Article %s doesn\'t exisist any more' % \
+            article_item[0])
         curs = conn.cursor()
         sql_string = "DELETE from g13_records" + \
             " WHERE article = ? and editor = ?;"
         curs.execute(sql_string,article_item)
         conn.commit()
         curs = None
-        pywikibot.output("Submission %s doesn't exisist." % article_item[0])
+        logger.debug('Article %s removed from DB' % \
+            article_item[0])
         continue
     if True == article.isRedirectPage():
         #Submission is now a redirect.  Happy Day, it got promoted to
         # article space!
+        logger.info('Article %s was promoted to mainspace' % \
+            article_item[0])
         curs = conn.cursor()
         sql_string = "DELETE from g13_records" + \
             " WHERE article = ? and editor = ?;"
         curs.execute(sql_string,article_item)
         conn.commit()
         curs = None
-        pywikibot.output("Submission % is now a redirect" % article_item[0])
+        logger.debug('Article %s was removed from DB' % \
+            article_item[0])
+        continue
+    article_text = article.get()
+    if '{{db-g13}}' not in article_text:
+        #Hrm... Article no longer has my CSD template on it. Remove it
+        # from the DB
+        logger.info('Article %s no longer has my csd template on it' % \
+            article.title())
+        curs = conn.cursor()
+        sql_string = "DELETE from g13_records" + \
+            " WHERE article = ? and editor = ?;"
+        curs.execute(sql_string,article_item)
+        conn.commit()
+        curs = None
+        logger.debug('Article %s was removed from DB' % \
+            article_item[0])
         continue
   conn.close()
+  logger.info('DB Maintenance completed')
 
 def main():
     g13_db_maintenance()
 
 if __name__ == "__main__":
+    logger = logging.getLogger('g13_maintenance_bot')
+    logger.setLevel(logging.DEBUG)
+    trfh = logging.handlers.TimedRotatingFileHandler('logs/g13_nudge', \
+        when='D', \
+        interval = 1, \
+        backupCount = 90, \
+    )
+    trfh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    trfh.setFormatter(formatter)
+    logger.addHandler(trfh)
     try:
         main()
     finally:
